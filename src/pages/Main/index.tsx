8819d88a8b820adffb7e253dedcce8ddd5194fe6
charilscah <chrlschwb77@gmail.com>
2023-02-13 19:38:25 -0800

    test version

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,14 +3,14 @@ import { Header } from '@/components'
 
 function Main() {
   return (
-    <div>
+    <div className='home_body'>
       <h1>JOY EXCHANGE ESCROW</h1>
-      <h3 className='text-center'>JOY is currently underway of being listed on reputable exchanges. Meanwhile, to accomodate the needs of early buyers, the Joystream DAO is going to provide escrow services for JOY buy/sells and prevent scamming.</h3>
-      <h3>Please follow the guidelines in order to prevent being scammed. We do not guarantee safety of funds if you do not follow the safety guidelines.
+      <h2 className='text-center'>JOY is currently underway of being listed on reputable exchanges. Meanwhile, to accomodate the needs of early buyers, the Joystream DAO is going to provide escrow services for JOY buy/sells and prevent scamming.</h2>
+      <h3 style={{ marginTop: "50px" }}>Please follow the guidelines in order to prevent being scammed. We do not guarantee safety of funds if you do not follow the safety guidelines.
         Join this discord server() and go to #joy-market channel and write how much to/from whom you want to sell/buy JOY at which rate. It is important to make this public and not in a private chat for your own safety.
         Then contact any of the escrow service providers below and send your funds. An escrow fee of 10% will be deducted. After both parties send the promised funds, they will be transferred to their respective counter parties.
       </h3>
-      <div>
+      <div style={{ marginTop: "80px" }}>
         <li>
           1. l1.media telegram handle:  <a href="https://test">test</a>    discord handle: ""
         </li>
